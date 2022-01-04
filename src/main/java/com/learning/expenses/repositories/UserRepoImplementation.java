5a361e88ed0aea3d62431d5222f26cc10b87db0d
Cebo N <cebonqweniso00@gmail.com>
2022-01-04 23:30:59 +0200

    add method to insert the newly created user

--- src/main/java/com/learning/expenses/repositories/UserRepoImplementation.java
+++ src/main/java/com/learning/expenses/repositories/UserRepoImplementation.java
@@ -2,15 +2,40 @@ package com.learning.expenses.repositories;
 
 import com.learning.expenses.domain.User;
 import com.learning.expenses.exceptions.EtAuthException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
+
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 
 @Repository
 public class UserRepoImplementation implements UserRepository{
 
+    private static final String SQL_INSERT = "INSERT INTO USERS(USER_ID, FIRST_NAME, LAST_NAME, EMAIL, PASSWORD) " +
+            "VALUES (NEXTVAL('USERS_SEQ'), ?, ?, ?, ?)";
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
     @Override
     public Integer create(String firstName, String lastName, String email, String password) throws EtAuthException {
-        return null;
+        try{
+            KeyHolder keyHolder = new GeneratedKeyHolder();
+            jdbcTemplate.update(con -> {
+                PreparedStatement ps = con.prepareStatement(SQL_INSERT, Statement.RETURN_GENERATED_KEYS);
+                ps.setString(1,firstName);
+                ps.setString(2,lastName);
+                ps.setString(3,email);
+                ps.setString(4,password);
+                return  ps;
+            },keyHolder);
+            return (Integer) keyHolder.getKeys().get("USER_ID");
+        }catch (Exception ex){
+            throw new EtAuthException("Error occurred while creating an account");
+        }
     }
 
     @Override
